c77c2824b52095e4a6404f7180d03cd614bc1459
Mücahit Fatih Parlağı <Fatihprlg@users.noreply.github.com>
2020-05-07 00:06:44 +0300

    First version
    struct definition for customers

---
+++ BankAutomation/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct musteri{
+    int musteriID;
+    char *isim;
+    char *soyisim;
+    struct{
+        int gun;
+        int ay;
+        int yil;
+    }dogumTarihi;
+    int musteriTipi; //ticari = 1, bireysel = 2
+};
+
+
+int main()
+{
+
+
+
+
+    return 0;
+}
